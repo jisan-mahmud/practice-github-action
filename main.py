00946ea26e9d35ae3336f4f4122b4e6b00c75d2f
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-11-29 12:42:10 +0600

    feat: added fastapi test and test workflow

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+import os
+import sys
 
 app = FastAPI()
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 @app.get('/')
 async def hello():
